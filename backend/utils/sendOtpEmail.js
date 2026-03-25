39deef94e7f55a4d541c2f958f3fdc1a749719f6
adarshc003 <adarshadhi2003a@gmail.com>
2026-03-26 01:07:03 +0530

    Fixed OTP

--- backend/utils/sendOtpEmail.js
+++ backend/utils/sendOtpEmail.js
@@ -1,24 +1,25 @@
 const nodemailer = require("nodemailer");
 
-const sendOtpEmail = async (to, otp) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
+const sendOtpEmail = async (to, otp) => {
   await transporter.sendMail({
     from: `"E-Cart" <${process.env.EMAIL_USER}>`,
     to,
     subject: "Your OTP for Login",
     html: `
-      <div style="font-family: Arial;">
+      <div>
         <h2>Your OTP</h2>
-        <p>Use the OTP below to login:</p>
-        <h1 style="letter-spacing:4px">${otp}</h1>
-        <p>This OTP is valid for 5 minutes.</p>
+        <h1>${otp}</h1>
+        <p>Valid for 5 minutes</p>
       </div>
     `,
   });
